eebbf10b6454b56b9d56d1e1bb230753f88e31e0
manzoorWayar <softwareengineerwayar@gmail.com>
2023-10-15 16:18:55 +0430

    ready to deploy

--- server.js
+++ server.js
@@ -231,7 +231,9 @@ const io = require("socket.io")(server, {
 
 io.on("connection", (socket) => {
     console.log(`user connected ${socket.id}`);
+    const roomId = uuidv4();
 
+    socket.emit("room-id", { roomId });
     socket.on("create-new-room", (data) => {
         createNewRoomHandler(data, socket);
     });
